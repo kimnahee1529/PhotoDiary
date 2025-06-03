67810166034cb36ee8440476ba30db0c1296a2f7
sunjoolee <sheisthepoem@naver.com>
2025-06-03 21:17:59 +0900

    feat: Google weather retrofit module
    Add Qualifier annotation to distinguish retrofit object

---
+++ app/src/main/java/com/todaylab/cleanarchtemplate/remote/di/GoogleWeatherRetrofitModule.kt
@@ -0,0 +1,48 @@
+package com.todaylab.cleanarchtemplate.remote.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object GoogleWeatherRetrofitModule {
+    private const val BASE_URL = "https://weather.googleapis.com"
+
+    @Singleton
+    @Provides
+    @GoogleWeatherRetrofit
+    fun provideRetrofit(
+    ): Retrofit =
+        Retrofit
+            .Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+            .client(
+                createOkHttpClient(),
+            ).build()
+
+    private fun createOkHttpClient(): OkHttpClient {
+        val loggingInterceptor =
+            HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }
+
+        return OkHttpClient
+            .Builder()
+            .connectTimeout(20, TimeUnit.SECONDS)
+            .readTimeout(20, TimeUnit.SECONDS)
+            .writeTimeout(20, TimeUnit.SECONDS)
+            .addNetworkInterceptor(loggingInterceptor)
+            .build()
+    }
+}
---
+++ app/src/main/java/com/todaylab/cleanarchtemplate/remote/di/Qualifiers.kt
@@ -0,0 +1,7 @@
+package com.todaylab.cleanarchtemplate.remote.di
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class GoogleWeatherRetrofit
