381ca3f5947fb0f4a7c69185c21abb2246391393
sunjoolee <sheisthepoem@naver.com>
2025-06-06 16:46:48 +0900

    feat: data layer weather repository implementation

---
+++ app/src/main/java/com/todaylab/cleanarchtemplate/data/impl/WeatherRepositoryImpl.kt
@@ -0,0 +1,29 @@
+package com.todaylab.cleanarchtemplate.data.impl
+
+import com.todaylab.cleanarchtemplate.data.local.WeatherLocalDataSource
+import com.todaylab.cleanarchtemplate.data.remote.WeatherRemoteDataSource
+import com.todaylab.cleanarchtemplate.data.toDomain
+import com.todaylab.cleanarchtemplate.domain.model.Weather
+import com.todaylab.cleanarchtemplate.domain.repository.WeatherRepository
+import javax.inject.Inject
+
+/**
+ * data layer
+ * weather repository implementation
+ */
+class WeatherRepositoryImpl @Inject constructor(
+    private val weatherRemoteDataSource: WeatherRemoteDataSource,
+    private val weatherLocalDataSource: WeatherLocalDataSource
+) : WeatherRepository {
+    override fun getCurrentWeather(): Weather {
+        // get weather from local data source if exists
+        val localWeather = weatherLocalDataSource.getCurrentWeather()
+        if (localWeather != null) return localWeather.toDomain()
+
+        // get weather from remote data source if not exists
+        // and save it to local data source
+        val remoteWeather = weatherRemoteDataSource.getCurrentWeather()
+        weatherLocalDataSource.saveCurrentWeather(remoteWeather)
+        return remoteWeather.toDomain()
+    }
+}
