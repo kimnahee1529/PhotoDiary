c80deebc9d17eb2b07e218af408728b7cd151334
sunjoolee <sheisthepoem@naver.com>
2025-06-07 08:22:10 +0900

    feat: remote datasource di module

---
+++ app/src/main/java/com/todaylab/cleanarchtemplate/remote/di/RemoteDataSourceModule.kt
@@ -0,0 +1,17 @@
+package com.todaylab.cleanarchtemplate.remote.di
+
+import com.todaylab.cleanarchtemplate.data.remote.WeatherRemoteDataSource
+import com.todaylab.cleanarchtemplate.remote.impl.WeatherRemoteDataSourceImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RemoteDataSourceModule {
+    @Binds
+    @Singleton
+    abstract fun bindWeatherRemoteDataSource(impl: WeatherRemoteDataSourceImpl): WeatherRemoteDataSource
+}
