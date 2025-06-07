617354bb96e97212df76cd31d2f6de4fcfb66110
sunjoolee <sheisthepoem@naver.com>
2025-06-07 10:10:07 +0900

    fix: fix google weather api service end point

--- app/src/main/java/com/todaylab/cleanarchtemplate/remote/api/GoogleWeatherApiService.kt
+++ app/src/main/java/com/todaylab/cleanarchtemplate/remote/api/GoogleWeatherApiService.kt
@@ -11,7 +11,7 @@ import retrofit2.http.Query
  * google weather api service interface
  */
 interface GoogleWeatherApiService {
-    @GET("currentConditions:lookup?")
+    @GET("currentConditions:lookup")
     suspend fun getCurrentWeather(
         @Query("key") apiKey: String = BuildConfig.GOOGLE_MAP_API_KEY,
         @Query("location.latitude") lat: Double,
