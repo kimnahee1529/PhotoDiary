bf58fd902af00a695ed274d0638290bee9f3583a
sunjoolee <sheisthepoem@naver.com>
2025-06-07 10:09:45 +0900

    feat: open weather api service

--- app/src/main/java/com/todaylab/cleanarchtemplate/remote/api/OpenWeatherApiService.kt
+++ app/src/main/java/com/todaylab/cleanarchtemplate/remote/api/OpenWeatherApiService.kt
@@ -1,3 +1,20 @@
 package com.todaylab.cleanarchtemplate.remote.api
 
-class OpenWeatherApiService
+import com.todaylab.cleanarchtemplate.BuildConfig
+import com.todaylab.cleanarchtemplate.remote.model.WeatherGoogleWeatherResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * remote layer
+ * open weather api service interface
+ */
+interface OpenWeatherApiService {
+    @GET("onecall")
+    suspend fun getCurrentWeather(
+        @Query("appid") apiKey: String = BuildConfig.OPEN_WEATHER_API_KEY,
+        @Query("lat") lat: Double,
+        @Query("lon") long: Double
+    ): Response<WeatherGoogleWeatherResponse>
+}
