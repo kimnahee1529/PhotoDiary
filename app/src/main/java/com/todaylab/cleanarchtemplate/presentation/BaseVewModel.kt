928eb4f38efa3d3387b9f348d9c44b0ad8074ca6
sunjoolee <sheisthepoem@naver.com>
2025-06-09 18:41:40 +0900

    feat: base viewmodel class with custom exception handling logic

---
+++ app/src/main/java/com/todaylab/cleanarchtemplate/presentation/BaseVewModel.kt
@@ -0,0 +1,37 @@
+package com.todaylab.cleanarchtemplate.presentation
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.plus
+
+abstract class BaseViewModel(
+    val savedStateHandle: SavedStateHandle,
+) : ViewModel() {
+    private val _customExceptions: MutableSharedFlow<CustomException> = MutableSharedFlow()
+    protected val customException = _customExceptions.asSharedFlow()
+
+    private val exceptionHandler =
+        CoroutineExceptionHandler { _, throwable ->
+            viewModelScope.launch {
+                throwable.let {
+                    if (it is CustomException) {
+                        it
+                    } else {
+                        CustomException(
+                            code = CustomException.StatusCode.Unknown,
+                            message = throwable.message ?: "",
+                        )
+                    }.let { customException ->
+                        _customExceptions.emit(customException)
+                    }
+                }
+            }
+        }
+
+    protected val viewModelScopeEH = viewModelScope + exceptionHandler
+}
