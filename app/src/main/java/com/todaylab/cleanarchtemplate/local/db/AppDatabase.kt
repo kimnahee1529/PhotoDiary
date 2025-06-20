8989c095c98b1ffe97199850cbea7ca033012301
sunjoolee <sheisthepoem@naver.com>
2025-06-20 19:24:32 +0900

    feat: add date type converter to room db

--- app/src/main/java/com/todaylab/cleanarchtemplate/local/db/AppDatabase.kt
+++ app/src/main/java/com/todaylab/cleanarchtemplate/local/db/AppDatabase.kt
@@ -2,6 +2,7 @@ package com.todaylab.cleanarchtemplate.local.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.todaylab.cleanarchtemplate.local.dao.WeatherDao
 import com.todaylab.cleanarchtemplate.local.model.WeatherLocal
 
@@ -10,6 +11,7 @@ import com.todaylab.cleanarchtemplate.local.model.WeatherLocal
  * room database
  */
 @Database(entities = [WeatherLocal::class], version = 1)
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun weatherDao(): WeatherDao
 }
