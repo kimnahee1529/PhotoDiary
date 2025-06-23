f47cc73b259e8cadef50450960a25764ed095743
sunjoolee <sheisthepoem@naver.com>
2025-06-23 23:23:54 +0900

    refactor: Remove http client module

--- app/src/main/java/com/todaylab/cleanarchtemplate/remote/di/WeatherRetrofitModule.kt
+++ app/src/main/java/com/todaylab/cleanarchtemplate/remote/di/WeatherRetrofitModule.kt
@@ -10,14 +10,14 @@ import dagger.hilt.components.SingletonComponent
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 /**
  * remote layer
  * di for open weather api retrofit
- *
- * https://openweathermap.org/api/one-call-3#current
  */
 @Module
 @InstallIn(SingletonComponent::class)
@@ -27,15 +27,26 @@ object WeatherRetrofitModule {
 
     @Singleton
     @Provides
-    fun provideRetrofit(
-        httpClient: OkHttpClient
-    ): Retrofit =
-        Retrofit
+    fun provideRetrofit(): Retrofit {
+        val loggingInterceptor =
+            HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }
+
+        val httpClient = OkHttpClient
+            .Builder()
+            .connectTimeout(20, TimeUnit.SECONDS)
+            .readTimeout(20, TimeUnit.SECONDS)
+            .writeTimeout(20, TimeUnit.SECONDS)
+            .addInterceptor(loggingInterceptor)
+            .addInterceptor(ApiKeyInterceptor("appid", APP_KEY))
+            .build()
+
+        return Retrofit
             .Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
-            .client(
-                httpClient.newBuilder().addInterceptor(ApiKeyInterceptor("appid", APP_KEY)).build()
-            )
+            .client(httpClient)
             .build()
+    }
 }
