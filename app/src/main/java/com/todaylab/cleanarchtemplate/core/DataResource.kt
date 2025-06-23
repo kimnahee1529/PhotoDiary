ab9b7e5778a4c7c61d901f671055ebe1ef6b521b
sunjoolee <sheisthepoem@naver.com>
2025-06-23 16:37:01 +0900

    feat: add DataResource sealed class

---
+++ app/src/main/java/com/todaylab/cleanarchtemplate/core/DataResource.kt
@@ -0,0 +1,14 @@
+package com.todaylab.cleanarchtemplate.core
+
+// todo: wrap data classes in sealed class
+sealed class DataResource<out T> {
+    class Success<T>(val data: T) : DataResource<T>()
+    class Error(val throwable: Throwable) : DataResource<Nothing>()
+    class Loading<T>(val data: T? = null) : DataResource<T>()
+
+    companion object {
+        fun <T> success(data: T) = Success(data)
+        fun error(throwable: Throwable) = Error(throwable)
+        fun <T> loading(data: T? = null) = Loading(data)
+    }
+}
