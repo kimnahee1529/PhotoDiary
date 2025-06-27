afce48d865de59f999f0a2f63abb376cf12b406f
sunjoolee <sheisthepoem@naver.com>
2025-06-27 13:38:23 +0900

    feat: check if weather date is within 3 hours

--- app/src/main/java/com/todaylab/cleanarchtemplate/data/local/WeatherLocalDataSource.kt
+++ app/src/main/java/com/todaylab/cleanarchtemplate/data/local/WeatherLocalDataSource.kt
@@ -1,8 +1,12 @@
 package com.todaylab.cleanarchtemplate.data.local
 
+import com.todaylab.cleanarchtemplate.core.DataResource
 import com.todaylab.cleanarchtemplate.data.model.WeatherEntity
 
 interface WeatherLocalDataSource {
-    suspend fun getWeather(lat: Double, lon: Double): WeatherEntity?
+    /**
+     * Get weather within 3 hours from local data source
+     */
+    suspend fun getFreshWeather(lat: Double, lon: Double): DataResource<WeatherEntity>?
     suspend fun saveWeather(weather: WeatherEntity)
 }
--- app/src/main/java/com/todaylab/cleanarchtemplate/local/impl/WeatherLocalDataSourceImpl.kt
+++ app/src/main/java/com/todaylab/cleanarchtemplate/local/impl/WeatherLocalDataSourceImpl.kt
@@ -1,25 +1,34 @@
 package com.todaylab.cleanarchtemplate.local.impl
 
+import com.todaylab.cleanarchtemplate.core.DataResource
 import com.todaylab.cleanarchtemplate.data.local.WeatherLocalDataSource
 import com.todaylab.cleanarchtemplate.data.model.WeatherEntity
 import com.todaylab.cleanarchtemplate.local.room.dao.WeatherDao
 import com.todaylab.cleanarchtemplate.local.toData
 import com.todaylab.cleanarchtemplate.local.toLocal
 import javax.inject.Inject
+import kotlin.math.abs
+import kotlin.time.Duration.Companion.hours
 
 class WeatherLocalDataSourceImpl @Inject constructor(
     private val weatherDao: WeatherDao
 ) : WeatherLocalDataSource {
-    override suspend fun getWeather(lat: Double, lon: Double): WeatherEntity? {
-        // get saved weather entity
-        val weather = weatherDao.getWeather() ?: return null
-        // todo: check if date, lat, lon matches
-//        if (weather.lat != lat || weather.long != long) {
-//            weatherDao.deleteWeather()
-//            return null
-//        }
+    override suspend fun getFreshWeather(lat: Double, lon: Double): DataResource<WeatherEntity> {
+        val savedWeather = weatherDao.getWeather() ?: return DataResource.empty()
 
-        return weather.toData()
+        val locationTolerance = 0.0001 // tolerance for lat/lon comparison
+        val isLocationMatch = abs(savedWeather.lat - lat) < locationTolerance &&
+                abs(savedWeather.lon - lon) < locationTolerance
+
+        val threeHoursAgo = System.currentTimeMillis() - 3.hours.inWholeMilliseconds
+        val isDateWithin3Hours = savedWeather.date.time >= threeHoursAgo
+
+        if (isLocationMatch && isDateWithin3Hours) {
+            return DataResource.success(savedWeather.toData())
+        } else {
+            weatherDao.deleteWeather()
+            return DataResource.empty()
+        }
     }
 
     override suspend fun saveWeather(weather: WeatherEntity) {
